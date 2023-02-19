f9af688cc47c831281c24705545fd3d38b5416bc
Anusha Surendran <anushasurendrankp333@gmail.com>
2023-02-19 21:38:32 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,28 @@
+
+import './App.css';
+import Model2 from './components/Model2';
+import SignIn from './components/SignIn';
+import LandingPage from './components/LandingPage';
+import SignUp from './components/SignUp';
+import LandingSignUp from './components/LandingSignUp';
+import FailAlertBar from './components/FailAlertBar';
+import PassAlertBar from './components/PassAlertBar';
+import Cards from './components/Cards';
+import SideBar from './components/SideBar';
+function App() {
+  return (
+    <div className="App">
+    {/* <Navbar/>
+      <LandingPage/> */}
+      {/* <SignUp/>
+      <LandingSignUp/> */}
+      {/* <Model2/> */}
+      <SideBar/>
+   
+      {/* <SignIn/>
+      <LandingPage/> */}
+    </div>
+  );
+}
+
+export default App;
